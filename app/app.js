f04cf70c01427459b1a11536f8e1c909de6df58c
Avlessi <a.avlessi@gmail.com>
2015-12-09 14:47:07 +0100

    update

--- app/app.js
+++ app/app.js
@@ -1,16 +1,47 @@
 "use strict";
 
-angular
+var myModule = angular
   .module('myApp', [
     'ngResource',
      'ngRoute',
     'ngSanitize',
     'ui.bootstrap'
-  ])
-  .controller('myCtrl', function($scope) {
+  ]);
+
+
+myModule.controller('myCtrl', function($scope) {
+	$scope.pupil = {"name":"Julien"};
     $scope.people = [
       { firstName: 'Misko', lastName: 'Hevery', company: 'Google' }
     ];
+});
+
+
+var questionsService = myModule.service('questionsService', function(){
+   
+});
+
+myModule.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/questions.html',
+        controller: 'QuestionsCtrl',
+        resolve: {
+          questions: function(questionsService) {
+            return questionsService.resolveQuestions();
+          }
+        }
+      })
+      .when('/questions/:questionId', {
+        templateUrl: 'views/question.html',
+        controller: 'QuestionCtrl',
+        resolve: {
+          question: function(questionsService) {
+            return questionsService.resolveCurrentQuestion();
+          }
+        }
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
   });
-
-
